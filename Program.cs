90302198e4590a8477a326b16b338ec85ca1a863
chuong9x <31106432+chuong9x@users.noreply.github.com>
2021-01-24 09:43:33 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExceptionTest();
+        Console.Read();
+    }
+    private static void ExceptionTest()
+    {
+        try
+        {
+            int j = 0;
+            int i = 5;
+            i = 1 / j;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            var stList = ex.StackTrace.ToString().Split('\\');
+            Console.WriteLine("Exception occurred at " + stList[stList.Count() - 1]);
+        }
+    }
+}
